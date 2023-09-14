c22f32b86df1ba08e81fb287d18dcaa9a4000d3e
Emily Reyes <emilymwind1@gmail.com>
2023-09-14 17:17:17 -0600

    check if entered word is found in dictionary

--- Wordle.py
+++ Wordle.py
@@ -13,7 +13,13 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 def wordle():
 
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        entered_word = s.lower()
+
+        #check if entered word is found in dictionary
+        if entered_word in FIVE_LETTER_WORDS:
+            gw.show_message("Great Choice!")
+        else:
+            gw.show_message("Not in word list.")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
@@ -22,8 +28,8 @@ def wordle():
     random_word = random.choice(FIVE_LETTER_WORDS).upper()
 
     # Display the chosen word in the first row of boxes (row 0)
-    for i in range(len(random_word)):
-        gw.set_square_letter(0, i, random_word[i])
+    # for i in range(len(random_word)):
+    #     gw.set_square_letter(0, i, random_word[i])
 
 # Startup code
 
